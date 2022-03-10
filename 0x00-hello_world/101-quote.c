0367b22a07216fd4e3e2964d48ee434343622254
maza123716 <mazafaith9919.com>
2022-03-10 07:08:20 -0800

    C program print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main  main block
+* Return: 1
+*/
+
+int main(void)
+{
+  char *s="and that piece of art is useful\" - Dora Korpar, 20  15-10-19\n";
+  while (*s) putchar(*s++);
+  return (1);
+} 
