b7007dc15fe9aa06b2542e224aaa1fffd30eea24
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-10 18:40:13 +0300

    C program print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
 */
 int main(void)
 {
-char *s = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-while (*s) putchar(*s++);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+while (*s) 
+putchar(*s++);
 return (1);
 } 
