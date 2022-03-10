dd466afff2884cb96665a7c00dbee5d624bec760
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-10 19:27:38 +0300

    C program print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,8 @@
 * main - Entry point
 * Return: Always 1 (Success)
 */
-int main(void)
-{
+int main(void) {
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-while (*s ) 
+while (*s) 
 putchar(*s++);
 return (1); }
