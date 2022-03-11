3ff4699e5f35948e8d8647dc5a5a098db812c1b5
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-11 22:11:54 +0300

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
-
- * main - main block
-
- * Description: prints all single digit numbers of base 10
-
- * starting from 0, followed by a new line.
-
- * Return: 0
-
- */
-
+* main - main block
+* Description: prints all single digit numbers of base 10
+* starting from 0, followed by a new line.
+* Return: 0
+*/
 int main(void)
-
 {
 
 	char d = '0';
