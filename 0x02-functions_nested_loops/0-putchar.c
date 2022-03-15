c905bf617916fa028c0f7a9f2caa4d01ce7cc9af
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-15 23:40:37 +0300

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,17 @@
  * Return: 0.
  */
 
-int main()
+int main(void)
 {
-    char ch = '_putchar';
-  
-    _putchar(ch);
-  
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
