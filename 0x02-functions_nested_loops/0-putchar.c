e43b71b52472d017f69557a5a69efd3fb8d28200
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-15 23:20:46 +0300

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,17 @@
  * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
+
 int main(void)
 {
-	char s[9] = "_putchar";
-	int i=0;
-
-	while (i<8)
-	{
-		_putchar(s[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	
 	 return (0);
 }
