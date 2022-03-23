1fb97b9fc5487aab8802bfda5a19985553f83140
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-23 22:55:04 +0300

    Function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,22 +8,15 @@
 
 void print_number(int n)
 {
-int divisor = 1, i, resp;
+	unsigned int x = n;
 
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-
-}
-
+	_putchar(x % 10 + '0');
 }
