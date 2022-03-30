fa5720b657e67d1853e690932bc1dd6fd3161545
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-03-30 21:10:49 +0300

    Inception

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,30 @@
 #include "main.h"
-
 /**
- * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
- * Return: int
+ * wildcmp - compare two strings with possible wildcards
+ * @s1: string
+ * @s2: string
+ * Return:int 0 or 1
  */
-
-int last_index(char *s)
+int wildcmp(char *s1, char *s2)
 {
-int n = 0;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (0);
 
-if (*s > '\0')
-	n += last_index(s + 1) + 1;
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
 
-return (n);
-}
-
-/**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
- * Return: 0 or 1
- */
-
-int is_palindrome(char *s)
-{
-int end = last_index(s);
-
-return (check(s, 0, end - 1, end % 2));
-}
-
-/**
- * check - checker for the palindrome
- * @s: string
- * @start: int moves from right to left
- * @end: int moves from left to right
- * @pair: int
- * Return: 0 or 1
- */
-
-
-int check(char *s, int start, int end, int pair)
-{
-
-if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
-	return (1);
-else if (s[start] != s[end])
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else if (wildcmp(s1, s2 + 1))
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
 	return (0);
-else
-	return (check(s, start + 1, end - 1, pair));
 }
