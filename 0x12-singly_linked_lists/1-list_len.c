a1355e578e0085b4c5a830cd089986b82e30745b
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-04-21 21:07:03 +0300

    Final codes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * @h: pointer
  * Return: number of nodes in the list
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t len = 0;
 	int i;
@@ -13,10 +13,6 @@ size_t print_list(const list_t *h)
 
 	for (i = 0; c; i++)
 	{
-		if (c->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", c->len, c->str);
 	c = c->next;
 	len++;
 	}
