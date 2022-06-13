86d56796d9da375b6a890d677ca6164e07414500
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-06-13 21:02:56 +0300

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/betascribbles/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/win.so https://github.com/maza123716/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
